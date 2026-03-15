63923b7c19efe05ec45ae2e17bfc1b60383e609e
azharuddinak47 <azharuddin.au543@gmail.com>
2026-03-15 22:03:24 +0600

    done array

---
+++ array.js
@@ -0,0 +1,12 @@
+const cars = ["Saab", "Volvo", "BMW"]; // array;
+// 0, 1, 2
+const car = "BMW";
+
+console.log(car);
+console.log(cars[1]);
+
+const numbers = [10, 20, 30, 40, 50];
+console.log(numbers[2]);
+
+
+// Home work: methods of array: push, pop, shift, unshift, length, indexOf, includes, slice, splice

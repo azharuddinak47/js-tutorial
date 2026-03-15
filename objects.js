839d31cae4e4797793bb9658e38a2870f5e861e2
azharuddinak47 <azharuddin.au543@gmail.com>
2026-03-15 22:09:11 +0600

    done object

---
+++ objects.js
@@ -0,0 +1,10 @@
+
+const user = { // storing multiple related data
+    name: "Azhar", // property: value
+    age: 30,
+    email: "azhar@example.com",
+    address: '123 Main St, Anytown, USA'
+}
+
+console.log(user); // print whole object
+console.log(user.age); // access single property

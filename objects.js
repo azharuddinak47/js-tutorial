801a87f4fee27d0dc2d5c60c4c5d1cc7cebdef44
azharuddinak47 <azharuddin.au543@gmail.com>
2026-03-15 22:13:14 +0600

    done array of object

--- objects.js
+++ objects.js
@@ -8,3 +8,20 @@ const user = { // storing multiple related data
 
 console.log(user); // print whole object
 console.log(user.age); // access single property
+
+
+// mix array with object it's called array of objects
+// const users = ["azhar", "ali", "john"];
+const users = [
+    {
+        name: "Azhar",
+        age: 30,
+    },
+    {
+        name: "Ali",
+        age: 25,
+    },
+]
+
+console.log(users[0].name); // access name of first user
+console.log(users[1].age); // access age of second user

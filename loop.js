a71226ac153f0951a9175150d53a24ee8750732d
azharuddinak47 <azharuddin.au543@gmail.com>
2026-03-15 21:46:22 +0600

    done loop

--- loop.js
+++ loop.js
@@ -1,7 +1,15 @@
-for (let i = 0; i < 5; i++) { // loop means do something multiple times
-    console.log("hi");
-}
+// for (let i = 0; i < 5; i++) { // loop means do something multiple times
+//     console.log("hi");
+// }
 
-for (let i = 0; i < 5; i++) { // i++ means i = i + 1 or increment i by 1
-
+for (let i = 1; i < 5; i = i + 1) { // i++ means i = i + 1 or increment i by 1
+    console.log("run");
 } 
+
+
+i = 0; // initialization
+i = 1
+i = 2
+i = 3
+i = 4
+// i = 5
